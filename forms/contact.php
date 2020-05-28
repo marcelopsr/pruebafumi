423dbe86b37df4182a8e871dbc557d9a0c46f4ae
marcelopsr <65307746+marcelopsr@users.noreply.github.com>
2020-05-28 10:23:14 -0400

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -25,7 +25,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                           // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                       // Enable SMTP authentication
     $mail->Username   = 'mesr.96@gmail.com';                        // SMTP username
-    $mail->Password   = '@Marcelo1996';                             // SMTP password
+    $mail->Password   = 'clave';                             // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;             // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                        // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
